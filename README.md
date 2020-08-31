638ebf76f5782979a356828925a895aa38747849
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-08-31 14:14:27 +0200

    övning 1.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Prog1-ovn
  Övningar i programmering 1
+
+Detta är ett övningsrepository föt kursen programering 1.
---
+++ Uppgift 1.4.py
@@ -0,0 +1,6 @@
+# uppgift1.4.py
+
+name = input('vad heter du? ')
+print('Hej', name)
+Surname = input ('Vad heter du i efternamn?')
+Print('hej', name surname)
