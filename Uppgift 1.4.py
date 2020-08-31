861fec477bd1cdaa110080bdb566d8f81344046d
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-08-31 14:43:06 +0200

    Kap2test

--- Uppgift 1.4.py
+++
@@ -1,6 +0,0 @@
-# uppgift1.4.py
-
-name = input('vad heter du? ')
-print('Hej', name)
-Surname = input ('Vad heter du i efternamn?')
-Print('hej', name surname)
---
+++ Uppgift1.4.py
@@ -0,0 +1,10 @@
+# uppgift1.4.py
+
+name = input('vad heter du? ')
+surname = input('vad heter du i efternamn?')
+
+print('Hej', name,'',surname)
+
+favfood = input('vad är din favouritmat?')
+
+print(favfood, 'är gott')
