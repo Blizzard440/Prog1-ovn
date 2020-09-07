0da966bcf2f38c30e032292d4f04bfd2ba04256c
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-09-07 14:44:23 +0200

    Update Uppg23.py
    update

--- Kap2uppg.py/Uppg23.py
+++ Kap2uppg.py/Uppg23.py
@@ -4,8 +4,8 @@ y=x*x
 print(f'talet i kvadrat är{y:.5f}')
 
 i=19
-j=107
+j=107000
 k=40
 print(f'resultat:{i}') #Betyder att bara 1 skrivs
 print(f'resultat:{j:5}') # Betyder 5 digits med 2 blanka i början
-print(f'resultat:{k:05}') #Betyder totalt 5 digits
+print(f'resultat:{k:05}') #Betyder totalt 5 digits med nollor
