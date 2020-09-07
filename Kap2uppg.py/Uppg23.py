d623e70b46dd176db22fd0bc58a6a87efb3be2be
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-09-07 14:38:58 +0200

    Create Uppg23.py
    uppg2.3

---
+++ Kap2uppg.py/Uppg23.py
@@ -0,0 +1,11 @@
+svar=input('skriv ett tal: ')
+x=float(svar)
+y=x*x
+print(f'talet i kvadrat är{y:.5f}')
+
+i=19
+j=107
+k=40
+print(f'resultat:{i}') #Betyder att bara 1 skrivs
+print(f'resultat:{j:5}') # Betyder 5 digits med 2 blanka i början
+print(f'resultat:{k:05}') #Betyder totalt 5 digits
