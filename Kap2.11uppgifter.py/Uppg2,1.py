c37e9585f1898de8c85e18626805c2b27ac1d9fd
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-09-28 14:35:08 +0200

    Create Uppg2,1.py

---
+++ Kap2.11uppgifter.py/Uppg2,1.py
@@ -0,0 +1,12 @@
+milnu=input('Mätarställning idag? ') #ställer fråga
+milinnan=input('Mätarställning för et år sedan? ') #ställer fråga
+literiår=input('Hur många liter har gått åt i år? ') #ställer fråga
+
+milnu = int(milnu) #bestämmer att milnu är en int variabel
+milinnan = int(milinnan) #bestämmer att milinnan är en int variabel
+körtiår = milnu-milinnan #räknar ut antal körda mil i år genom att subtrahera antalet mil innan från antalet mil på mätaren nu
+literiår = float(literiår) #bestämmer att literiår är en float variabel
+literpermil = literiår/körtiår #bestämmer att literpermil är en variabel som literiår delat i körtiår
+
+print(f'Antal mil körda i år: {körtiår:.0f}') #skriver svar
+print(f'Bilen har dragit {literpermil:.3f} per mil i år.') #skriver svar
