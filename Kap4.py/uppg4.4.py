32c79987ec634c465386daf956f939c1a4c3fb49
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-11-16 14:51:14 +0100

    2020-11/16

--- Kap4.py/uppg4.4.py
+++ Kap4.py/uppg4.4.py
@@ -3,7 +3,7 @@ starthöjd = float(input('Hur högt släpps bollen från? '))
 cm = starthöjd * 100
 ggr = 0
 
-while cm >= 1:
+while cm > 1:
     cm = cm * 0.7
     ggr = ggr + 1
     print (f'{cm:.2f} cm')
--- Kap4.py/uppg4.5.py
+++ Kap4.py/uppg4.5.py
@@ -1,10 +1,13 @@
-starthöjd = float(input('Hur högt släpps bollen från? '))
+starthöjd = 1
+while starthöjd > 0:
+    starthöjd = float(input('Hur högt släpps bollen från? '))
 
-cm = starthöjd * 100
-ggr = 0
+    cm = starthöjd * 100
+    ggr = 0
 
-while cm >= 1:
-    cm = cm * 0.7
-    ggr = ggr + 1
-    print (f'{cm:.2f} cm')
-print(f'bollen studsar {ggr} gånger')
+    while cm > 1:
+        cm = cm * 0.7
+        ggr = ggr + 1
+        print (f'{cm:.2f} cm')
+    print(f'bollen studsar {ggr} gånger')
+print('Tack för mig')
